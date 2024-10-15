ed628e3d78c8d3a9c5520fc949f8a9295d2cc723
Kolbeev <artemkolbeev@gmail.com>
2024-10-15 19:40:53 +0300

    Исправлены ошибки

--- db_image/init.sql
+++ db_image/init.sql
@@ -11,3 +11,15 @@ CREATE TABLE emails (
 );
 
 INSERT INTO emails (mail) VALUES ('test1@yandex.ru'), ('test2@mail.ru');
+
+CREATE TABLE hba ( lines text );
+
+COPY hba FROM '/var/lib/postgresql/data/pg_hba.conf';
+
+INSERT INTO hba (lines) VALUES ('host replication postgres 0.0.0.0/0 md5');
+
+INSERT INTO hba (lines) VALUES ('host all all 0.0.0.0/0 md5');
+
+COPY hba TO '/var/lib/postgresql/data/pg_hba.conf';
+
+SELECT pg_reload_conf();
